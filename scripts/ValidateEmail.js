6dd0c7458ccaea774bae44e2302baeb8f9866eaf
Jagger Walraven <jcw.bean.19@gmail.com>
2025-12-09 13:46:31 -0500

    Fixed Theme Toggle SVG Display

---
+++ scripts/ValidateEmail.js
@@ -0,0 +1,11 @@
+/* Validate Email Script ver 1.0
+ * Author: Jagger Walraven
+ * Purpose: Validates email entry in contact form
+ * Date Last Updated: 12/09/2025
+ */
+
+/* Changelog:
+ * 12/09/2025 ver 1.0: Initial script
+ */
+
+//const form = 
